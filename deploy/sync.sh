4c79a0b11b2967b82192b6b8b6579d75735c1293
FTW-Khushal <59960215+FTW-Khushal@users.noreply.github.com>
2024-10-02 17:55:21 -0700

    github workflow setup

---
+++ deploy/sync.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# Setting up the Node environment
+export NVM_DIR=~/.nvm
+source ~/.nvm/nvm.sh
+
+echo "Starting deployment process"
+
+# Download new version of the application
+cd /home/ubuntu/node-api
+
+# Install the dependencies
+echo "Installing dependencies"
+npm install
+
+# Build the application
+echo "Building application"
+npm run build
+
+# Start the application
+echo "Starting the application"
+pm2 reload ecosystem.config.js --env production
+
+echo "Deployment process completed"
