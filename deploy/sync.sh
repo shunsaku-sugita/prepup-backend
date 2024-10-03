67f5060865cb93dece586a5d19b7d321b1756308
shunsaku-sugita <saku.dot@gmail.com>
2024-10-02 20:12:10 -0700

    github work-flow update

--- deploy/sync.sh
+++ deploy/sync.sh
@@ -1,24 +1,17 @@
 #!/bin/bash
-
 # Setting up the Node environment
 export NVM_DIR=~/.nvm
 source ~/.nvm/nvm.sh
-
 echo "Starting deployment process"
-
 # Download new version of the application
-cd /home/ubuntu/node-api
-
+cd /home/ubuntu/node-api || { echo "Failed to change directory"; exit 1; }
 # Install the dependencies
 echo "Installing dependencies"
-npm install
-
+sudo npm install || { echo "npm install failed"; exit 1; }
 # Build the application
 echo "Building application"
-npm run build
-
+sudo npm run build || { echo "npm run build failed"; exit 1; }
 # Start the application
 echo "Starting the application"
-pm2 reload ecosystem.config.js --env production
-
+sudo pm2 reload ecosystem.config.js --env production || { echo "pm2 reload failed"; exit 1; }
 echo "Deployment process completed"
