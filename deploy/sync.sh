06bc1cfcd599c5e9f37509f1550a4d1e8ab1b8dd
Khushal Khunt <59960215+FTW-Khushal@users.noreply.github.com>
2024-10-07 15:27:45 -0700

    Update sync.sh

--- deploy/sync.sh
+++ deploy/sync.sh
@@ -6,15 +6,20 @@ source ~/.nvm/nvm.sh
 
 echo "Starting deployment process"
 
-echo "Adding environment variables"
-export PORT=${PORT}
-export TOKEN_KEY=${TOKEN_KEY}
-export DB_CONN_STRING=${DB_CONN_STRING}
-export DB_NAME=${DB_NAME}
-export USERS_COLLECTION_NAME=${USERS_COLLECTION_NAME}
-export MAIL_HOST=${MAIL_HOST}
-export MAIL_USERNAME=${MAIL_USERNAME}
-export MAIL_PASSWORD=${MAIL_PASSWORD}
+# Create or overwrite .env file and add environment variables
+ENV_FILE="/home/ubuntu/node-api/.env"
+
+echo "Creating .env file with environment variables..."
+{
+    echo "PORT=$PORT"
+    echo "TOKEN_KEY=$TOKEN_KEY"
+    echo "DB_CONN_STRING=$DB_CONN_STRING"
+    echo "DB_NAME=$DB_NAME"
+    echo "USERS_COLLECTION_NAME=$USERS_COLLECTION_NAME"
+    echo "MAIL_HOST=$MAIL_HOST"
+    echo "MAIL_USERNAME=$MAIL_USERNAME"
+    echo "MAIL_PASSWORD=$MAIL_PASSWORD"
+} > "$ENV_FILE"
 
 # Download new version of the application
 cd /home/ubuntu/node-api || { echo "Failed to change directory"; exit 1; }
@@ -29,5 +34,5 @@ sudo npm run build || { echo "npm run build failed"; exit 1; }
 
 # Start the application
 echo "Starting the application"
-sudo -E pm2 reload ecosystem.config.js --env production || { echo "pm2 reload failed"; exit 1; }
+sudo pm2 reload ecosystem.config.js --env production || { echo "pm2 reload failed"; exit 1; }
 echo "Deployment process completed"
