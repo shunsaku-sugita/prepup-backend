673f88e3cb9582d2f50d4d98bb4878a715c66944
Khushal Khunt <59960215+FTW-Khushal@users.noreply.github.com>
2024-10-07 16:18:19 -0700

    Update sync.sh

--- deploy/sync.sh
+++ deploy/sync.sh
@@ -6,15 +6,19 @@ source ~/.nvm/nvm.sh
 
 echo "Starting deployment process"
 
-echo "Adding environment variables"
-export PORT=${PORT}
-export TOKEN_KEY=${TOKEN_KEY}
-export DB_CONN_STRING=${DB_CONN_STRING}
-export DB_NAME=${DB_NAME}
-export USERS_COLLECTION_NAME=${USERS_COLLECTION_NAME}
-export MAIL_HOST=${MAIL_HOST}
-export MAIL_USERNAME=${MAIL_USERNAME}
-export MAIL_PASSWORD=${MAIL_PASSWORD}
+# Create or overwrite .env file and add environment variables
+ENV_FILE="/home/ubuntu/node-api/.env"
+echo "Creating .env file with environment variables..."
+{
+    echo "PORT=$PORT"
+    echo "TOKEN_KEY=$TOKEN_KEY"
+    echo "DB_CONN_STRING=$DB_CONN_STRING"
+    echo "DB_NAME=$DB_NAME"
+    echo "USERS_COLLECTION_NAME=$USERS_COLLECTION_NAME"
+    echo "MAIL_HOST=$MAIL_HOST"
+    echo "MAIL_USERNAME=$MAIL_USERNAME"
+    echo "MAIL_PASSWORD=$MAIL_PASSWORD"
+} > "$ENV_FILE"
 
 # Download new version of the application
 cd /home/ubuntu/node-api || { echo "Failed to change directory"; exit 1; }
